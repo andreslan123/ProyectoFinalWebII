edee1f7aa5bd4864117f4222ccd3cda55e8b79c6
andreslan123 <andressanticla@gmail.com>
2026-06-09 14:15:27 -0400

    completando los models

--- app/Models/Carrito.php
+++ app/Models/Carrito.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Carrito extends Model
 {
     use HasFactory;
+
+    protected $table = 'carritos';
+    
+    // Solo permitimos llenar de forma masiva el ID del usuario dueño del carrito
+    protected $fillable = ['user_id'];
+
+    // Relación: Un carrito tiene muchos detalles (productos adentro)
+    public function detalles()
+    {
+        return $this->hasMany(CarritoDetalle::class);
+    }
 }
