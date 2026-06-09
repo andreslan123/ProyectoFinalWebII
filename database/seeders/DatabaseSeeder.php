45ec6e90e885bb74e6f96400df96e430284b9870
Juan Miguel Huanca <miguelhuanca676@gmail.com>
2026-06-09 15:41:10 -0400

    borrando algunos comentarios no debidos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,25 +3,22 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;       // 👈 ¡No olvides importar tu modelo User aquí arriba!
-use App\Models\Producto;   // 👈 ¡Importa también tu modelo Producto!
+use App\Models\User;       
+use App\Models\Producto;   
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        // 1. Primero se ejecutan las tablas con datos fijos de tu compañero
         $this->call([
             RolSeeder::class,
             CategoriaSeeder::class,
             MarcaSeeder::class,
             SubcategoriaSeeder::class,
             ProveedorSeeder::class,
-            // Quitamos ProductoSeeder de aquí porque ahora lo hará tu Factory 👇
         ]);
 
-        // 2. ¡Aquí das la orden para que los factories creen los datos masivos!
-        User::factory(10)->create();     // Fabrica 10 usuarios aleatorios en la base de datos
-        Producto::factory(50)->create(); // Fabrica 50 productos aleatorios con Faker
+        User::factory(10)->create();    
+        Producto::factory(50)->create(); 
     }
 }
