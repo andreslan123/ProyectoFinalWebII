f884b50187af614de37cbe27f3104be5c1ceb987
Juan Miguel Huanca <miguelhuanca676@gmail.com>
2026-06-12 21:36:31 -0400

    resolviendo conflictos de kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,11 +6,6 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-<<<<<<< HEAD
- 
-=======
-    
->>>>>>> e1e492fc563b76326ce9ef87f824930a56d77135
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
@@ -19,38 +14,24 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        
     ];
 
-<<<<<<< HEAD
- 
-=======
-    
->>>>>>> e1e492fc563b76326ce9ef87f824930a56d77135
     protected $middlewareGroups = [
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-        // ¡BORRA LA LÍNEA QUE TENÍAS AQUÍ!
     ];
 
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> e1e492fc563b76326ce9ef87f824930a56d77135
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
@@ -62,9 +43,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'check.role' => \App\Http\Middleware\CheckRole::class,
-<<<<<<< HEAD
-        
-=======
->>>>>>> e1e492fc563b76326ce9ef87f824930a56d77135
     ];
 }
